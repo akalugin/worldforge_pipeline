9c4187ae98f1847e634a3b9cd26b7bbd1cab74e5
akalugin <a.kalugin@live.com>
2014-10-04 09:13:12 -0700

    separated the tools in to panels
    Added some additional tools

--- blender/wf_tool_panel.py
+++ blender/wf_tool_panel.py
@@ -16,60 +16,105 @@ def get_armature (name):
             return ob
     return False
 
-class PANEL_OT_wf_tools_panel(bpy.types.Panel):
+class PANEL_OT_wf_tools(bpy.types.Panel):
+    bl_space_type = "VIEW_3D"
+    bl_region_type = "TOOLS"
+    bl_category = "WorldForge"
+    bl_label = "Tools"
+    bl_options = {"DEFAULT_CLOSED"}
+    
+    def draw(self, context):
+        scene = context.scene
+        layout = self.layout
+
+        col = layout.column(align=True)
+        col.label(text="Rename Objects:")
+        layout.prop(scene, "wf_rename_panel") 
+        row = layout.row()
+        row.operator('object.wf_rename_objects', text='Renamer', icon="FILE_TICK")
+        col = layout.column(align=True)
+        col.operator('object.wf_pivot_to_selected', text='', icon='FORCE_FORCE')
+        row = col.row(align=True)
+        row.operator("object.shade_smooth", text="Smooth")
+        row.operator("object.shade_flat", text="Flat")
+
+class PANEL_OT_wf_mat_panel(bpy.types.Panel):
+    bl_space_type = "VIEW_3D"
+    bl_region_type = "TOOLS"
+    bl_category = "WorldForge"
+    bl_label = "Material Utils"
+    bl_options = {"DEFAULT_CLOSED"}
+    
+    def draw(self, context):
+        scene = context.scene
+        layout = self.layout
+
+        row = layout.row()
+        # layout.qlabel(text="Material Utilities")
+        row = layout.row(align=True)
+        row.operator('mesh.wf_fix_materials', text= 'Fix Materials', icon='SCULPTMODE_HLT')
+        row.operator('scene.wf_open_ogre_materials', text='Ogre Mats', icon='IMASEL')
+        # col = layout.column(align=True)
+        row = layout.row(align=True)
+        row.operator('view3d.material_to_texface',text = 'Mat to Tex', icon='MATERIAL_DATA')
+        row.operator('view3d.texface_to_material',text = 'Tex to Mat', icon='FACESEL_HLT')
+
+class PANEL_OT_wf_rigging_panel(bpy.types.Panel):
+    bl_space_type = "VIEW_3D"
+    bl_region_type = "TOOLS"
+    bl_category = "WorldForge"
+    bl_label = "Rigging Utils"
+    bl_options = {"DEFAULT_CLOSED"}
+    
+    def draw(self, context):
+        scene = context.scene
+        layout = self.layout
+        col = layout.row()
+        col.operator('object.vertex_group_limit_total', text= 'Limit Weights')
+        col.operator('object.clean_vertex_groups', text= 'Clean Weights')
+
+class PANEL_OT_wf_ogre_export(bpy.types.Panel):
     """Worldforge Tools Panel"""
     bl_label = "WorldForge Tools"
     bl_space_type = 'VIEW_3D'
     bl_region_type = 'TOOLS'
     bl_category = "WorldForge"
     # bl_context = "objectmode"
-    bl_label = "WorldForge"
+    bl_label = "Ogre Export"
+    bl_options = {"DEFAULT_CLOSED"}
 
     def draw(self, context):
         layout = self.layout
 
         scene = context.scene
-        scn = context.scene
-        
-        obj = get_armature ('wf_armature')
-#------------------------- WF OGRE EXPORTER SECTION ----------------------------------
-        layout.label(text="OGRE Object Exporter")
+
+
         row = layout.row()
-        row.operator("mesh.wf_export_ogre_static")
-        
-        layout.separator()
-        layout.separator()
-        layout.label(text="OGRE Animation Exporter")
+        row.operator("mesh.wf_export_ogre_static", icon='VIEW3D')
+
 
         row = layout.row(align=True)
         row.prop(scene, "frame_start")
         row.prop(scene, "frame_end")
         col = layout.column() 
+        obj = get_armature ('wf_armature')
         
         col.prop_search(scene, "Rig", bpy.data, "armatures")
         row = layout.row()
-        row.operator("mesh.wf_export_ogre_animated")
+        row.operator("mesh.wf_export_ogre_animated", icon='BONE_DATA')
 
-#--------------------- ANIMATION AND RIGGIN UTILITES ----------------------------------
-        row = layout.row()
-        layout.label(text="Weight Utilities")
-        row = layout.row()
-        row.operator('object.vertex_group_limit_total', text= 'Limit Weights')
-        row = layout.row()
-        row.operator('object.clean_vertex_groups', text= 'Clean Weights')
 
-#------------------------ WF OGRE MATERIAL UTILITES -----------------------------------
-        row = layout.row()
-        layout.label(text="Material Utilities")
-        row = layout.row(align=True)
-        row.operator('mesh.wf_fix_materials', text= 'Fix Materials')
-        row.operator('scene.wf_open_ogre_materials', text= 'Open Ogre Mats')
-        
 def register():
-    bpy.utils.register_class(PANEL_OT_wf_tools_panel)
+    bpy.utils.register_class(PANEL_OT_wf_ogre_export)
+    bpy.utils.register_class(PANEL_OT_wf_mat_panel)
+    bpy.utils.register_class(PANEL_OT_wf_rigging_panel)
+    bpy.utils.register_class(PANEL_OT_wf_tools)
 
 def unregister():
-    bpy.utils.unregister_class(PANEL_OT_wf_tools_panel)
+    bpy.utils.unregister_class(PANEL_OT_wf_ogre_export)
+    bpy.utils.unregister_class(PANEL_OT_wf_mat_panel)
+    bpy.utils.unregister_class(PANEL_OT_wf_rigging_panel)
+    bpy.utils.unregister_class(PANEL_OT_wf_tools)
 
 if __name__ == "__main__":
     register()
